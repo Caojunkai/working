41635d195129b1c88f445ddc476d66556bc24650
Laratrvel <353290933@qq.com>
2015-12-15 19:16:02 +0800

    sadsad

--- resource/admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `#__resource_welder`;
-DROP TABLE IF EXISTS `#__resource_gas`;
-DROP TABLE IF EXISTS `#__resource_welding`;
-DROP TABLE IF EXISTS `#__resource_tech`;
-DROP TABLE IF EXISTS `#__resource_workshop`;
-DROP TABLE IF EXISTS `#__resource_machine`;
-DROP TABLE IF EXISTS `#__resource_maintain`;
