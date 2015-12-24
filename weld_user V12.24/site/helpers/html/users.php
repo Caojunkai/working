03da7a813de5e0f86e2c503f69806376b8f427b0
Laratrvel <353290933@qq.com>
2015-12-24 17:23:12 +0800

    add weld_user component

---
+++ weld_user V12.24/site/helpers/html/users.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_weldusers
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Extended Utility class for the Users component.
+ *
+ * @since  2.5
+ */
+class JHtmlUsers
+{
+	/**
+	 * Display an image.
+	 *
+	 * @param   string  $src  The source of the image
+	 *
+	 * @return  string  A <img> element if the specified file exists, otherwise, a null string
+	 *
+	 * @since   2.5
+	 */
+	public static function image($src)
+	{
+		$src = preg_replace('#[^A-Z0-9\-_\./]#i', '', $src);
+		$file = JPATH_SITE . '/' . $src;
+
+		jimport('joomla.filesystem.path');
+		JPath::check($file);
+
+		if (!file_exists($file))
+		{
+			return '';
+		}
+
+		return '<img src="' . JUri::root() . $src . '" alt="" />';
+	}
+
+	
+
+	/**
+	 * Build an array of block/unblock user states to be used by jgrid.state,
+	 * State options will be different for any user
+	 * and for currently logged in user
+	 *
+	 * @param   boolean  $self  True if state array is for currently logged in user
+	 *
+	 * @return  array  a list of possible states to display
+	 *
+	 * @since  3.0
+	 */
+	public static function blockStates( $self = false)
+	{
+		if ($self)
+		{
+			$states = array(
+				1 => array(
+					'task'           => 'unblock',
+					'text'           => '',
+					'active_title'   => 'COM_USERS_USER_FIELD_BLOCK_DESC',
+					'inactive_title' => '',
+					'tip'            => true,
+					'active_class'   => 'unpublish',
+					'inactive_class' => 'unpublish',
+				),
+				0 => array(
+					'task'           => 'block',
+					'text'           => '',
+					'active_title'   => '',
+					'inactive_title' => 'COM_USERS_USERS_ERROR_CANNOT_BLOCK_SELF',
+					'tip'            => true,
+					'active_class'   => 'publish',
+					'inactive_class' => 'publish',
+				)
+			);
+		}
+		else
+		{
+			$states = array(
+				1 => array(
+					'task'           => 'unblock',
+					'text'           => '',
+					'active_title'   => 'COM_USERS_TOOLBAR_UNBLOCK',
+					'inactive_title' => '',
+					'tip'            => true,
+					'active_class'   => 'unpublish',
+					'inactive_class' => 'unpublish',
+				),
+				0 => array(
+					'task'           => 'block',
+					'text'           => '',
+					'active_title'   => 'COM_USERS_USER_FIELD_BLOCK_DESC',
+					'inactive_title' => '',
+					'tip'            => true,
+					'active_class'   => 'publish',
+					'inactive_class' => 'publish',
+				)
+			);
+		}
+
+		return $states;
+	}
+
+	/**
+	 * Build an array of activate states to be used by jgrid.state,
+	 *
+	 * @return  array  a list of possible states to display
+	 *
+	 * @since  3.0
+	 */
+	public static function activateStates()
+	{
+		$states = array(
+			1 => array(
+				'task'           => 'activate',
+				'text'           => '',
+				'active_title'   => 'COM_USERS_TOOLBAR_ACTIVATE',
+				'inactive_title' => '',
+				'tip'            => true,
+				'active_class'   => 'unpublish',
+				'inactive_class' => 'unpublish',
+			),
+			0 => array(
+				'task'           => '',
+				'text'           => '',
+				'active_title'   => '',
+				'inactive_title' => 'COM_USERS_ACTIVATED',
+				'tip'            => true,
+				'active_class'   => 'publish',
+				'inactive_class' => 'publish',
+			)
+		);
+
+		return $states;
+	}
+}
---
+++ weld_user V12.24/site/views/group/tmpl/edit.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+// Include the component HTML helpers.
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+
+JHtml::_('behavior.formvalidator');
+JHtml::_('formbehavior.chosen', 'select');
+
+JFactory::getDocument()->addScriptDeclaration("
+	Joomla.submitbutton = function(task)
+	{
+		if (task == 'group.cancel' || document.formvalidator.isValid(document.getElementById('group-form')))
+		{
+			Joomla.submitform(task, document.getElementById('group-form'));
+		}
+	};
+
+	Joomla.twoFactorMethodChange = function(e)
+	{
+		var selectedPane = 'com_users_twofactor_' + jQuery('#jform_twofactor_method').val();
+
+		jQuery.each(jQuery('#com_users_twofactor_forl;                                                                                              ms_container>div'), function(i, el) {
+			if (el.id != selectedPane)
+			{
+				jQuery('#' + el.id).hide(0);
+			}
+			else
+			{
+				jQuery('#' + el.id).show(0);
+			}
+		});
+	};
+");
+$fieldsets = $this->form->getFieldsets();
+?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_weldusers&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="group-form" class="form-validate form-horizontal">
+	<?php echo JLayoutHelper::render('joomla.edit.item_title', $this); ?>
+	<fieldset>
+		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>
+
+		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'details', JText::_('用户组设置', true)); ?>
+
+
+		<legend><?php echo JText::_('COM_USERS_USERGROUP_DETAILS');?></legend>
+		<div class="control-group">
+			<div class="control-label">
+				<?php echo $this->form->getLabel('title'); ?>
+			</div>
+			<div class="controls">
+				<?php echo $this->form->getInput('title'); ?>
+			</div>
+		</div>
+		<div class="control-group">
+			<?php $parent_id = $this->form->getField('parent_id');?>
+			<?php if (!$parent_id->hidden) : ?>
+				<div class="control-label">
+					<?php echo $parent_id->label; ?>
+				</div>
+			<?php endif;?>
+			<div class="controls">
+				<?php echo $parent_id->input; ?>
+			</div>
+		</div>
+
+		<?php echo JHtml::_('bootstrap.endTab'); ?>
+
+
+		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'levels', JText::_('用户组访问级别设置', true)); ?>
+
+
+
+
+
+		<?php echo JHtml::_('bootstrap.endTab'); ?>
+		<?php echo JHtml::_('bootstrap.endTabSet'); ?>
+
+
+
+
+
+	</fieldset>
+	<button type="submit" class="btn btn-default btn-primary"style="margin-left: 200px;margin-right: 30px" ><span class="icon-save"></span>提交</button>
+	<input type="hidden" name="task" value="group.save">
+	<a href="index.php?option=com_weldusers&view=groups">	<button type="button" class="btn"><span class="icon-cancel"></span>取消</button></a>
+	</div>
+	<?php echo JHtml::_('form.token'); ?>
+</form>
